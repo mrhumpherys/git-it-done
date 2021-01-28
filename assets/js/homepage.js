66a3c26a312e0f799cff6814137de29239a6ca99
Michael Humpherys <michaelhumpherys@Michaels-MacBook-Air.local>
2021-01-28 15:23:55 -0500

    finished language functionality

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 
 var formSubmitHandler = function(event) {
@@ -16,6 +16,20 @@ var formSubmitHandler = function(event) {
         alert("Please enter a GitHub username");
     }
 };
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    });
+}
 
 
 var getUserRepos = function(user) {
@@ -82,6 +96,17 @@ var displayRepos = function(repos, searchTerm) {
     console.log(repos);
     console.log(searchTerm);
 };
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+
+    if(language) {
+    getFeaturedRepos(language);
+
+    repoContainerEl.textContent = "";
+    };
+}
 
 
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
 userFormEl.addEventListener("submit", formSubmitHandler);
