50b95b4e12a82473e6be7b44c7cd530744016d36
Michael Humpherys <michaelhumpherys@Michaels-MacBook-Air.local>
2021-01-28 13:44:39 -0500

    closed out list issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,17 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if (repoName) {
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+    }else {
+        document.location.replace("./index.html");
+    }
+}
 
 var getRepoIssues = function(repo) {
     var aprUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -16,7 +28,7 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!")
+            document.location.replace("./index.html");
         }
     });
 };
@@ -67,4 +79,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 }
 
-getRepoIssues("facebook/react");
+getRepoName();
